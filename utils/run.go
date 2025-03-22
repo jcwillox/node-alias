e691918a77e2701331d691957e5021058f68b2fb
Josh Willox <joshwillox@gmail.com>
2025-03-22 13:01:18 +1100

    feat: use exec on linux and send kill signal on windows

--- utils/run.go
+++ utils/run.go
@@ -6,8 +6,19 @@ import (
 	"github.com/spf13/cobra"
 	"golang.org/x/sys/execabs"
 	"os"
+	"os/signal"
+	"runtime"
 	"strings"
+	"syscall"
+	"time"
 )
+
+func logErr(err error) {
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+}
 
 func RunCommand(name string, args []string, remaining ...string) {
 	RunAliasCommand(name, name, args, remaining...)
@@ -18,19 +29,46 @@ func RunAliasCommand(name string, alias string, args []string, remaining ...stri
 
 	_, _ = fmt.Fprintf(os.Stderr, "\x1b[90m$ %s %s\x1b[0m\n", alias, strings.Join(args, " "))
 
-	cmd := execabs.Command(name, args...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	if runtime.GOOS != "windows" {
+		exePath, err := execabs.LookPath(name)
+		if err != nil {
+			logErr(err)
+		}
+		err = syscall.Exec(exePath, append([]string{name}, args...), os.Environ())
+		if err != nil {
+			logErr(err)
+		}
+	} else {
+		cmd := execabs.Command(name, args...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
 
-	err := cmd.Run()
-	if err != nil {
-		var exitErr *execabs.ExitError
-		if errors.As(err, &exitErr) {
-			os.Exit(exitErr.ExitCode())
-		} else {
-			fmt.Println("error:", err)
-			os.Exit(1)
+		err := cmd.Start()
+		if err != nil {
+			logErr(err)
+		}
+
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+
+		go func() {
+			<-sigs
+			time.Sleep(time.Millisecond * 100)
+			err := cmd.Process.Kill()
+			if err != nil {
+				logErr(err)
+			}
+		}()
+
+		err = cmd.Wait()
+		if err != nil {
+			var exitErr *execabs.ExitError
+			if errors.As(err, &exitErr) {
+				os.Exit(exitErr.ExitCode())
+			} else {
+				logErr(err)
+			}
 		}
 	}
 }
